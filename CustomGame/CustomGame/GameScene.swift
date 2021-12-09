26a0a4c2337fe9ca9677869fcb40aaad78f96490
aoifem00 <amcmanu4@binghamton.edu>
2021-12-09 15:34:12 -0500

    Made wheel spin

---
+++ CustomGame/CustomGame/GameScene.swift
@@ -0,0 +1,79 @@
+//
+//  GameScene.swift
+//  CustomGame
+//
+//  Created by Aoife McManus on 12/9/21.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        let wheelTexture=SKTexture.init(imageNamed:"Image")
+        //let playerSpriteTexture=SKTexture(imageNamed:"Image-4");
+        let wheel=SKSpriteNode(texture:wheelTexture)
+        
+        wheel.position=CGPoint(x: 0, y: 0)
+        wheel.size.width=500
+        wheel.size.height=500
+        self.addChild(wheel)
+        
+        let action = SKAction.rotate(byAngle: CGFloat(Double.pi * 2), duration: 1)
+        wheel.run(action)
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.green
+            self.addChild(n)
+        }
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.blue
+            self.addChild(n)
+        }
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.red
+            self.addChild(n)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let label = self.label {
+            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        }
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
